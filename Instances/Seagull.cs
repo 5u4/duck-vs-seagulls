4e1e990912b7f0757e22fbef84b8635cc88b3f6b
Sen Hung Wong <alexwongsenhung@gmail.com>
2020-06-10 22:43:56 -0700

    Add seagull

---
+++ Instances/Seagull.cs
@@ -0,0 +1,45 @@
+using Godot;
+using System;
+
+public class Seagull : KinematicBody2D
+{
+    private readonly float DELTA_STEP = 100;
+    private float flyCooldown;
+
+    [Export]
+    public float gravity = 300;
+    [Export]
+    public float speed = 75;
+    [Export]
+    public float flyHeight = 45;
+    [Export]
+    public float maxFlyCooldown = 30;
+
+    public Vector2 velocity;
+
+    public override void _PhysicsProcess(float delta)
+    {
+        ApplyGravity(delta);
+        HandleFly(delta);
+        HandleMovement();
+    }
+
+    private void ApplyGravity(float delta)
+    {
+        velocity.y += gravity * delta;
+    }
+
+    private void HandleMovement()
+    {
+        velocity = MoveAndSlide(velocity, Vector2.Up);
+    }
+
+    private void HandleFly(float delta)
+    {
+        flyCooldown -= delta * DELTA_STEP;
+        if (flyCooldown > 0) return;
+
+        flyCooldown = maxFlyCooldown;
+        velocity.y = -flyHeight;
+    }
+}
